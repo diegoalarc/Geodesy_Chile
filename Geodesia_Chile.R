d2f2f558f05e9b0dee62c96ded7efecbb4739729
diegoalarc <diego.alarcondiaz@gmail.com>
2020-12-02 22:06:31 +0100

    Formulas geodesicas

---
+++ Geodesia_Chile.R
@@ -0,0 +1,49 @@
+# Elipsoide
+
+names_elip <- c('PSAD-56',	'SAD-69',	'WGS-84',	'GRS-80 (SIRGAS)')
+a <-	c(6378388,	6378160,	6378137,	6378137)
+divF <- 	c(297,	298.25,	298.257223563,	298.257222101)
+
+# Funtions
+
+f <- function(x) {
+  1/x
+}
+
+b <- function(x,y) {
+  x-1/y*x
+}
+
+e <- function(x,y) {
+  1-((x-1/y*x)^2/x^2)
+}
+
+e2 <- function(x,y) {
+  (x^2/(x-1/y*x)^2)-1
+}
+
+Elipsoide <- as.data.frame(cbind(names_elip, a, divF, f(divF), b(a,divF), e(a,divF),e2(a,divF)))
+names(Elipsoide) <- c("ELIPSOIDES", "a", "1/f", "f", "b", "e^2", "e´^2")
+
+# Lat
+
+g <- -33
+m <- 38
+s <- 30.123456
+
+# Long
+
+g1 <- -70
+m1 <- 41
+s1 <- 35.123456
+
+sexagesimal <- function(x,y,z){
+  x-(y/60)-(z/3600)
+}
+
+radianes <- function(x,y,z){
+  (x-(y/60)-(z/3600))*pi/180
+}
+
+sexagesimal(g,m,s)
+radianes(g,m,s)
